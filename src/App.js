83eca2d724d8f195ddf8594aa6e752e2d39678d4
Iskandarbek <iskandarbekergashev@gmail.com>
2021-05-06 10:26:19 +0500

    card update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-// import Card from './components/Card/Card';
-import Accordion from './components/Accordion/Accordion';
+import Card from './components/Card/Card';
 
 function App(props) {
 
   return (
-    <div className="App">
-      <Accordion title="Lorem ipsum dolor" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
-      <Accordion title="Lorem ipsum dolor" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
-      <Accordion title="Lorem ipsum dolor" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." />
-      
-      {/* <Card /> */}
+    <div className="App">      
+      <Card />
     </div>
   );
 }
